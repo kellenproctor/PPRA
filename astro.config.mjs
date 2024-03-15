62032d60534ec55ddc575b6ead517a94ab3e36ec
Kellen Proctor <kellenproctor@gmail.com>
2024-03-14 21:10:37 -0700

    cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,24 @@ import vercelStatic from '@astrojs/vercel/static';
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: 'https://screwfast.uk',
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
   prefetch: true,
-  integrations: [tailwind(), sitemap(), compressor({ gzip: false, brotli: true })],
+  integrations: [tailwind(), sitemap(), compressor({
+    gzip: false,
+    brotli: true
+  })],
   output: 'static',
   experimental: {
     clientPrerender: true,
     directRenderScript: true
   },
-  adapter: vercelStatic()
+  adapter: cloudflare()
 });
